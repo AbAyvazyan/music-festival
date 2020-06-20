9ea15ce9c1c176274a3a7187043040a487f5e456
AbAyvazyan <abayvazyan_webdev@gmail.com>
2020-06-20 13:32:00 +0400

    cg

--- Js/media.js
+++ Js/media.js
@@ -1,45 +1,45 @@
 $(window).on("load",function(){
-    $(".animate").fadeOut();
+  $(".animate").fadeOut();
 });
 
 $(window).on('beforeunload', function() {
-  $(window).scrollTop(0);
+$(window).scrollTop(0);
 });
 
 
 $(document).ready(function(){
-    $(window).on("scroll",function(){
+  $(window).on("scroll",function(){
 
-        if ($(this).scrollTop() > 50) {
-          $(".top").css({"opacity":1});
-        }else{
-          $(".top").css({"opacity":0});
-        }
+      if ($(this).scrollTop() > 50) {
+        $(".top").css({"opacity":1});
+      }else{
+        $(".top").css({"opacity":0});
+      }
 
-      })
+    })
 
-      $(".alone").on("click",function(){
-        $("html").css("cursor","zoom-out");
-        $(".cover").css("display","block");
-        $(".coverimage").css({"display":"block","cursor":"default"}).attr("src",$(this).attr("src"));
-        $(".exit").css("display","block");
-       $("html").css({"margin": 0, "height": "100%", "overflow": "hidden"});
-      })
+    $(".alone").on("click",function(){
+      $("html").css("cursor","zoom-out");
+      $(".cover").css("display","block");
+      $(".coverimage").css({"display":"block","cursor":"default"}).attr("src",$(this).attr("value"));
+      $(".exit").css("display","block");
+     $("html").css({"margin": 0, "height": "100%", "overflow": "hidden"});
+    })
 
 $(".cover").click(function(event) {                    
-        if(!$(event.target).closest('.coverimage').length)
-        {
-        $("html").css("cursor","default");
-        $(".cover").css("display","none");
-        $(".coverimage").css("display","none").attr("src","");
-        $(".exit").css("display","none");
-        $("html").css({"margin": 0, "height": "", "overflow": ""});               
-        }
-     }); 
+      if(!$(event.target).closest('.coverimage').length)
+      {
+      $("html").css("cursor","default");
+      $(".cover").css("display","none");
+      $(".coverimage").css("display","none").attr("src","");
+      $(".exit").css("display","none");
+      $("html").css({"margin": 0, "height": "", "overflow": ""});               
+      }
+   }); 
 
-     $(".navbar").animate({"margin-bottom":"0"},1000);
+   $(".navbar").animate({"margin-bottom":"0"},1000);
 
-       $(".top").on("click",function(){
-        $("html").animate({ scrollTop: 0 }, 1500);
-      });
+     $(".top").on("click",function(){
+      $("html").animate({ scrollTop: 0 }, 1500);
     });
+  });
